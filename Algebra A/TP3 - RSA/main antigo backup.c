5bc20a764e4be9f432adaaab9dd23f77e692110d
brenopoggiali <brenopoggiali@gmail.com>
2018-12-11 12:14:47 -0200

    Add test case and redoing tp3

---
+++ Algebra A/TP3 - RSA/main antigo backup.c
@@ -0,0 +1,233 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include <gmp.h>
+
+//check if p is prime
+long long isPrime(long long p){
+
+    long long i;
+    double j;
+    
+    j = sqrt(p);
+
+
+    for(i = 2; i <= j; i++){
+        //if the number isn't prime
+        if(p%i == 0)
+            return 0;
+    }
+
+    //if is prime
+    return 1;
+}
+
+//smallest number different than p and q that divides phi
+long long chooseE(long long phi, long long p, long long q, long long n){
+
+    long long i, e;
+    for(i = 65537; i < phi; i++){
+
+        if(phi%i != 0 && isPrime(i) && i != p && i != q){
+            e = i;
+            break;
+        }
+    }
+
+    return e;
+}
+
+//calculate the quotient and rest of a division
+void division(long long *rest, long long *quotient, long long a, long long b){
+
+    if(a >= 0){
+
+        *quotient = 0;
+        *rest = a;
+
+        while(*rest >= b){
+
+            *rest -= b;
+            *quotient = *quotient + 1;
+        }
+    }
+
+}
+
+//Calculate MDC and return the beta -> e^-1 (mod phi) = d
+long long extendedMDC(long long a, long long b){
+    
+    long long rest, quotient, xB = 1, yB = 0, x = 0, y = 1, alpha, beta, phi;
+    phi = a;
+
+    rest = a;
+    while(rest != 0){
+        division(&rest, &quotient, a, b);
+        a = b;
+        b = rest;
+        if(rest > 0){
+            alpha = xB - quotient *x;
+            beta = yB - quotient * y;
+
+            xB = x;
+            yB = y;
+            x = alpha;
+            y = beta;
+        }
+    }
+
+    if(beta < 0)
+        beta = phi + beta;
+
+    return beta;
+
+}
+
+//Calculate the P where a^e is congruent to P (mod n) 
+long power(long long a, long long e, long long n){
+    
+    long long A = a, P = 1, E = e;
+    
+    while(1){
+
+        //end of expansion -> return P
+        if(E == 0)
+            return P;
+
+        //if E is odd 
+        else if(E%2 != 0){
+            //doing the expansion (mod n) of each one of the multiplications
+            P = (A * P)%n;
+            E = (E-1)/2;
+        }
+
+        //if E is even
+        else{
+            E = E/2;
+        }
+        
+        A = (A*A)%n;
+    }
+    
+}
+
+//codify a string to ASCII
+void codifica(long long r, const char *str){
+    for (int i = 0; str[i] != '\n'; ++i){
+        (int) str[i];
+    }
+}
+
+//decodify a string from ASCII
+char *decodifica(const long long n){
+
+    
+
+}
+
+//encrypt a string of char using the rest of a division a^e(mod n) for each character using ASCII table
+int *criptografa(char *message, long long e, long long n){
+    
+    long long i;
+    int *messageC;
+
+    messageC = malloc(500 * sizeof(long long));
+    for(i = 0; i < 500; i++){
+        messageC[i] = power(message[i], e, n);
+    }
+
+    return messageC;
+}
+
+//decrypt a vector of int in a string of char using the rest of a division a^e(mod n) for each int
+char *descriptografa(int *messageC, long long d, long long n){
+    
+    long long i;
+    char *message;
+
+    message = malloc(500 * sizeof(char));
+    
+    for(i = 0; i < 500; i++){
+        message[i] = power(messageC[i], d, n);
+    }
+
+    return message;
+}
+
+void gera_chaves(long long *n, long long *e, long long *d){
+    // need to asign random number
+    long long p = 773;
+    while(isPrime(p) != 1){
+        p = rand();
+    }
+    //need to asign random number
+    long long q = 929;
+    while(isPrime(q) != 1 || q == p){
+        q = rand();
+    }
+
+    //Calculate n 
+    *n = p * q;
+    
+    //calculate euler coficient
+    long long phi = (p - 1)*(q - 1);
+
+    //choose e for private key
+    *e = chooseE(phi, p, q, *n);
+    
+    //choose d for public key
+    *d = extendedMDC(phi, *e);  
+}
+
+int main(void){
+
+    long long i;
+    long long p, q, n, phi, e, d;
+
+    //max number of char at message
+    char message[500];
+    int *messageC;
+
+
+    //read message to be encrypted
+    printf("Type a message: \n");
+    //clean the buffer
+    scanf("\n");
+    fgets(message, 500, stdin);
+
+    gera_chaves(&n, &e, &d);
+    
+
+    printf("Private key: (%llu, %llu)\n", e, n);
+    printf("Public key: (%llu, %llu)\n", d, n);
+
+    
+    codifica(n, message);
+
+    //encrypt message
+    messageC = criptografa(message, e, n);
+    
+    printf("\n");
+
+    //prints encrypted message
+    printf("Encrypted message: ");
+
+    for(i = 0; i < strlen(message); i++){
+        printf("%c", messageC[i]);
+    }
+    printf("\n\n"); 
+
+    //decrypt a message
+    char *messageD;
+    messageD = descriptografa(messageC, d, n);
+
+    printf("Decrypted message: %s\n\n", messageD);
+
+    //free allocated memory
+    free(messageC);
+    free(messageD);
+    return 0;
+
+}
--- Algebra A/TP3 - RSA/main.c
+++ Algebra A/TP3 - RSA/main.c
@@ -1,122 +1,268 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
+#include <gmp.h> 
 
-//Function return MDC between a and b
-int MDC(int a, int b){
-	if (b == 0)
-		return a;
-	else
-		return MDC(b, a%b);
+//g = MDC(a,b), a*x + b*y = g
+void mdc_estendido(mpz_t g, mpz_t x, mpz_t y, const mpz_t a, const mpz_t b){
+	//calculate x and y
+	if (mpz_cmp_ui(a, 0) == 0){
+		mpz_set_ui(x, 0);
+		mpz_set_ui(y, 1);
+		return;
+	}
+
+	mpz_t(x1); mpz_init(x1);
+	mpz_t(y1); mpz_init(y1);
+	mpz_t(r); mpz_init(r);
+	mpz_t(q); mpz_init (q);
+	mpz_t(rop); mpz_init (rop);
+	mpz_t(rop2); mpz_init (rop2);
+
+	mpz_mod(r, b, a); // r = b%a
+
+	mdc_estendido(g, x1, y1, r, a); // recursive
+
+	mpz_fdiv_q(q, b, a);
+	mpz_mul(rop, q, x1);
+
+	//*x = y1 - (b/a) * x1;
+	mpz_sub(x, y1, rop);
+	//*y = x1;
+	mpz_set(y, x1);
+
+	mpz_mul(rop, a, x);
+	mpz_mul(rop2, b, y);
+
+	mpz_add(g, rop, rop2);
+
+	mpz_clear (q);
+	mpz_clear (r);
+	mpz_clear (rop);
+	mpz_clear (rop2);
+	mpz_clear (x1);
+	mpz_clear (y1);
+
+	return;
 }
-//Function return aux that b^e is congruent to aux (mod n)
-int exp_binaria(int b, int e, int n){ 
-    int aux = 1;
 
-    b = b%n;
+// a*r is congruent to 1 (mod n)
+int inverso_modular(mpz_t r, const mpz_t a, const mpz_t n){
+    mpz_t(x);mpz_init(x);
+    mpz_t(y);mpz_init(y);
 
-    while(e>0){
-    	//e = odd
-    	if (e%2 == 1){
-    		aux = (aux*b)%n;
-    	}
+    mdc_estendido(r,x,y,a,n);
 
-    	//e = even
-    	e /= 2;
-    	b = (b*b)%n;
+    //if inverso modular is true (x is number that makes a*x = 1 (mod n))
+    if(mpz_cmp_ui(r,1) == 0){
+        //x = modular inverse of a mod n
+        mpz_set(r,x); // r = x;
+        if(mpz_sgn(r) == -1)
+        	mpz_add(r, n, r);
+
+        mpz_clear(x);
+        mpz_clear(y);
+        return 1;
     }
-
-    return aux;
+    else{
+        mpz_clear(x);
+        mpz_clear(y);
+        return 0;
+    }
 }
-//a^n-1 is congruent to 1 (mod n)??
-int talvez_primo(const int a, const int n, const int n1, unsigned int t, const int q){
-		if (n < 2){
-			return 0;
-		}
-		if (n == 2){
-			return 1;
-		}
-		int aux1 = a;
-		if (a > n){
-			aux1 = 2;
-		}
 
-		int aux2 = 0;
-		srand(time(NULL));
-		
-		for (int i = 0; i < 50; ++i){
-			if (MDC(aux1,n) != 1)
-				return 0;
-			if(exp_binaria(aux1, n1, n) != 1)
-				return 0;
-			aux1 = 2 + rand()%(n-4); 
-		}
+//b^e is conguent to r (mond n)
+void exp_binaria(mpz_t r, const mpz_t b, const mpz_t e, const mpz_t n){ 
+    if(mpz_cmp_ui(e,1) == 0){
+        mpz_mod(r,r,n);
+    }
+    else{
+        mpz_mod(r,b,n);
+        mpz_t mod; mpz_init(mod);
+        mpz_t q; mpz_init(q);
+        mpz_mod_ui(mod,e,2);
+        if(mpz_cmp_ui(mod,0) == 0){
+            mpz_fdiv_q_ui(q,e,2);
+            exp_binaria(r,b,q,n);
+            mpz_mul(r,r,r);
+            mpz_mod(r,r,n);
+            
+        }
+        else{
+            mpz_fdiv_q_ui(q,e,2);
+            exp_binaria(r,b,q,n);
+            mpz_mul(r,r,r);
+            mpz_mul(r,b,r);
+            mpz_mod(r,r,n);
+        }
+        mpz_clear(mod);
+        mpz_clear(q);
+    }
+}
 
+//a^(n-1)=(2^t*q) is congruent to 1 (mod n)?? if it is -> inconclusive
+int talvez_primo(const mpz_t a, const mpz_t n, const mpz_t n1, unsigned int t, const mpz_t q){
+	
+	if(mpz_cmp_si(n, 2) == -1){ // n < 2 
+		return 0;
+	}
+	else if(mpz_cmp_si(n, 2) == 0){ // n == 2 
 		return 1;
 	}
 
-int geranerate_random_prime1(){
-	srand(time(NULL));
-	int p = rand()%32740;
+	mpz_t(r); mpz_init(r);
+	mpz_t(i); mpz_init(i);
+	mpz_t(n1_var); mpz_init(n1_var);
+	mpz_t(n1_mod_2); mpz_init(n1_mod_2);
+	mpz_t(base); mpz_init(base);
+	mpz_t (d); mpz_init(d);
+    
 
-	while(talvez_primo(2, p, p-1, 1, 1) != 1){
-		p = rand()%32740;
+    mpz_set_ui(d,2); // d = 2
+	mpz_set(n1_var, n1); // n1_var = n1;
+	mpz_mod_ui(n1_mod_2, n1_var, 2); // n1_mod_2 = n1_var%2
+	mpz_mod(base, a, n); // base = a%n;
+	
+	if(mpz_cmp_si(n1_mod_2, 1) == 0){ // n == 2 
+		return 0;
+	}
+	if (mpz_cmp(base, n) == 0 || mpz_cmp_ui(base, 0) == 0){
+		mpz_set_ui(base, 2);
+	}
+	while(mpz_cmp_si(n1_mod_2, 0) == 0){ // n1_var % 2 == 0 (n1_var is even)
+		t++;
+		mpz_fdiv_q_ui(n1_var, n1_var, 2);
+		mpz_mod_ui(n1_mod_2, n1_var, 2);
 	}
 
-	return p;
-}
+	exp_binaria(r, base, n1_var, n); // r = (base)^(n1_var) (mod n)
 
-int geranerate_random_prime2(){
-	srand(time(NULL));
-	int q = 11 + rand()%32730;
-
-	while(talvez_primo(2, q, q-1, 1, 1) != 1){
-		q = 11 + rand()%32730;
+	while(1){
+		if((mpz_cmp_ui(i, 0) == 0 && mpz_cmp_ui(r, 1) == 0) || (mpz_cmp_ui(i, 0) >= 0 && mpz_cmp(r, n1) == 0)){
+			mpz_clear(r);
+			mpz_clear(i);
+			mpz_clear(n1_var);
+			mpz_clear(n1_mod_2);
+			mpz_clear(base);
+			mpz_clear(d);
+			return 1;
+		}
+		
+		mpz_add_ui(i, i, 1);
+		exp_binaria(r, r, d, n);
+		
+		if (mpz_cmp_ui(i, t) >= 0){
+			mpz_clear(r);
+			mpz_clear(i);
+			mpz_clear(n1_var);
+			mpz_clear(n1_mod_2);
+			mpz_clear(base);
+			mpz_clear(d);
+			return 0;
+		}
 	}
+} 
 
-	return q;
+void numero_aleatorio(mpz_t r, const mpz_t n, gmp_randstate_t rnd) {
+	mp_bitcnt_t num_bits = mpz_sizeinbase(n, 2);
+	do{
+		mpz_urandomb(r, rnd, num_bits);
+	}while (!(mpz_cmp_ui(r, 1) >= 0 && mpz_cmp(r, n) <= 0));
 }
 
-void gera_chaves(double *n, double *e, double *d){
-	//2 random primes between [2, 32740)	
-	int p = geranerate_random_prime1();
-	int q = geranerate_random_prime2();
+int provavelmente_primo(const mpz_t n, unsigned int iter, gmp_randstate_t rnd){
+	mpz_t(n1); mpz_init(n1);
+	mpz_t(base); mpz_init(base);
+	mpz_t(q_aux); mpz_init(q_aux);
+
+	mpz_sub_ui(n1, n, 1); // n1 = n-1;
+
+	for (int i = 0; i < iter; ++i){
+		numero_aleatorio(base, n, rnd);
+		if(talvez_primo(base, n, n1, 0, q_aux) == 0){
+			mpz_clear(n1);
+			mpz_clear(base);
+			mpz_clear(q_aux);
+			return 0;
+		}
+	}
+	mpz_clear(n1);
+	mpz_clear(base);
+	mpz_clear(q_aux);
+	return 1;
+}
+
+void primo_aleatorio(mpz_t r, unsigned int b, gmp_randstate_t rnd){
+	do{
+		mpz_urandomb(r, rnd, b);
+	}while(provavelmente_primo(r, 20, rnd) == 0);
+
+	return;
+}
+
+void gera_chaves(mpz_t n, mpz_t e, mpz_t d, gmp_randstate_t rnd){
+	mpz_t(p); mpz_init(p);
+	mpz_t(q); mpz_init(q);
+	mpz_t(p1); mpz_init(p1);
+	mpz_t(q1); mpz_init(q1);
+	mpz_t(phi); mpz_init(phi);
+	mpz_t(i); mpz_init(i);
+	mpz_t(gcd); mpz_init(gcd);
+	mpz_t(x); mpz_init(x);
+	mpz_t(y); mpz_init(y);
 
 
-	//public key
-	*n = p*q;
-	int phi = (p-1)*(q-1);
-	while(*e < phi){
-		if(MDC(*e, phi) == 1){
+	primo_aleatorio(p, 2048, rnd);
+	primo_aleatorio(q, 2048, rnd);
+	mpz_sub_ui(p1, p, 1);
+	mpz_sub_ui(q1, q, 1);
+
+	mpz_mul(n, p, q);
+	mpz_mul(phi, p1, q1);
+
+	for(mpz_set_ui(i, 65537); mpz_cmp(i, phi); mpz_add_ui(i, i, 1)){
+		mdc_estendido(gcd, x, y, i, phi);
+		if (mpz_cmp_ui(gcd, 1) == 0){
+			mpz_set(e, i);
 			break;
 		}
-		else
-			*e += 1 ;
 	}
 
-	//private key
-	//k -> arbitrary
-	int k = 2;
-	//d -> d*e = 1 + k*phi
-	*d = (1 + (k*phi))/ *e;
+	mdc_estendido(gcd, x, y, e, phi);
+	mpz_set(d, x);
+
+
+	mpz_clear(p);
+	mpz_clear(q);
+	mpz_clear(p1);
+	mpz_clear(q1);
+	mpz_clear(phi);
+	mpz_clear(i);
+	mpz_clear(gcd);
+	mpz_clear(x);
+	mpz_clear(y);
 }
 
 int main(int argc, char const *argv[]){
-	double n=2, e=65537, d=2;
-	double msg = 20;
+	mpz_t(n); mpz_init(n);
+	mpz_t(e); mpz_init(e);
+	mpz_t(d); mpz_init(d);
 
-	gera_chaves(&n, &e, &d);
+	gmp_randstate_t (rnd);
+	gmp_randinit_default(rnd);
+	gmp_randseed_ui(rnd, 19416511);
 
-	printf("n = p*q = %.0lf\n", n);
-	printf("e = public key = %.0lf\n", e);
-	printf("d = private key = %.0lf\n", d);
+	printf("\n\nQuestion 1:\n");
+	gera_chaves(n, e, d, rnd);
+		gmp_printf("Public key: (%Zd, %Zd)\n", n, e);
+		gmp_printf("Private key: (%Zd, %Zd)\n", n, d);
 
-	printf("\n");
+	printf("\n\nQuestion 2:\n");
+	printf("\n\nQuestion 3:\n");
+	printf("\n\nQuestion 4:\n");
+	printf("\n\nQuestion 5:\n");
 
-	printf("Message to encrypt: %lf\n", msg);
-	int c = exp_binaria((int)msg, e, n)
-	printf("Message encrypted: %lf\n", c);
-	printf("Message decripted\n");
+	mpz_clear(n);
+	mpz_clear(e);
+	mpz_clear(d);
 	return 0;
 }
