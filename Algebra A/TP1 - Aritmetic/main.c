c055693014ad2d29b950b3c7fd65002e4001eeee
brenopoggiali <brenopoggiali@gmail.com>
2018-12-11 03:33:26 -0200

    Fix bugs

--- Algebra A/TP1 - Aritmetic/main.c
+++ Algebra A/TP1 - Aritmetic/main.c
@@ -1,103 +1,159 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <gmp.h> 
 
 //Question 1
-int MDC(int a, int b){
-	if (b == 0)
-		return a;
-	else
-		return MDC(b, a%b);
-}
-int mdc_estendido(int g, int *x, int *y, const int a, const int b){
+void mdc_estendido(mpz_t g, mpz_t x, mpz_t y, const mpz_t a, const mpz_t b){
 	//calculate x and y
-	if (a == 0){
-		*x = 0;
-		*y = 1;
-		return b;
+	if (mpz_cmp_ui(a, 0) == 0){
+		mpz_set_ui(x, 0);
+		mpz_set_ui(y, 1);
+		return; // return b in int
 	}
 
-	int x1, y1;
-	int mdc = mdc_estendido(g, &x1, &y1, b%a, a);
+	mpz_t(x1); mpz_init(x1);
+	mpz_t(y1); mpz_init(y1);
+	mpz_t(r); mpz_init(r);
+	mpz_t(q); mpz_init (q);
+	mpz_t(rop); mpz_init (rop);
+	mpz_t(rop2); mpz_init (rop2);
 
-	*x = y1 - (b/a) * x1;
-	*y = x1;
+	mpz_mod(r, b, a); // r = b%a
 
-	return mdc;
-}
-int mpz_gcd(int g, int a, int b){
-	//check if mdc of a and b is = g
-	if (MDC(a,b) == g)
-		return 1;
-	else
-		0;
-}
-void mpz_gcdext(int g, int x, int y, const int a, const int b){
+	mdc_estendido(g, x1, y1, r, a); // recursive
 
+	mpz_fdiv_q(q, b, a);
+	mpz_mul(rop, q, x1);
+
+	//*x = y1 - (b/a) * x1;
+	mpz_sub(x, y1, rop);
+	//*y = x1;
+	mpz_set(y, x1);
+
+	mpz_mul(rop, a, x);
+	mpz_mul(rop2, b, y);
+
+	mpz_add(g, rop, rop2);
+
+	mpz_clear (q);
+	mpz_clear (r);
+	mpz_clear (rop);
+	mpz_clear (rop2);
+	mpz_clear (x1);
+	mpz_clear (y1);
+
+	return; //return mdc; in int
 }
+
+
 
 //Question 2
-int inverso_modular(int *r, const int a, const int n){
-	for (int i = 0; i < n; ++i){
-		if((a*i)%n == 1){
-			*r = i;
-			return 1;
-		}
-	}
-	return 0;
+int inverso_modular(mpz_t r, const mpz_t a, const mpz_t n){
+    mpz_t(x);mpz_init(x);
+    mpz_t(y);mpz_init(y);
+
+    mdc_estendido(r,x,y,a,n);
+
+    //if inverso modular is true (x is number that makes a*x = 1 (mod n))
+    if(mpz_cmp_ui(r,1) == 0){
+        //x = modular inverse of a mod n
+        mpz_set(r,x); // r = x;
+        if(mpz_sgn(r) == -1)
+        	mpz_add(r, n, r);
+
+        mpz_clear(x);
+        mpz_clear(y);
+        return 1;
+    }
+    else{
+        mpz_clear(x);
+        mpz_clear(y);
+        return 0;
+    }
 }
 
+
 //Question 3
-int exp_binaria(int r, const int b, const int e, const int n){ 
-    r = 1;
-    int x = b;
-    int y = e;
-
-    x = x%n;
-
-    while(y>0){
-    	//y = impar
-    	if (y%2 == 1){
-    		r = (r*x)%n;
-    	}
-
-    	//e = par
-    	y /= 2;
-    	x = (x*x)%n;
+void exp_binaria(mpz_t r, const mpz_t b, const mpz_t e, const mpz_t n){ 
+    if(mpz_cmp_ui(e,1) == 0){
+        mpz_mod(r,r,n);
     }
-
-    return r;
+    else{
+        mpz_mod(r,b,n);
+        mpz_t mod; mpz_init(mod);
+        mpz_t q; mpz_init(q);
+        mpz_mod_ui(mod,e,2);
+        if(mpz_cmp_ui(mod,0) == 0){
+            mpz_fdiv_q_ui(q,e,2);
+            exp_binaria(r,b,q,n);
+            mpz_mul(r,r,r);
+            mpz_mod(r,r,n);
+            
+        }
+        else{
+            mpz_fdiv_q_ui(q,e,2);
+            exp_binaria(r,b,q,n);
+            mpz_mul(r,r,r);
+            mpz_mul(r,b,r);
+            mpz_mod(r,r,n);
+        }
+        mpz_clear(mod);
+        mpz_clear(q);
+    }
 }
 
 
 
 int main(int argc, char const *argv[]){
-	int a, b, e, g, n, r, x, y;
+	mpz_t(a); mpz_init(a);
+	mpz_t(b); mpz_init(b);
+	mpz_t(e); mpz_init(e);
+	mpz_t(g); mpz_init(g);
+	mpz_t(n); mpz_init(n);
+	mpz_t(r); mpz_init(r);
+	mpz_t(x); mpz_init(x);
+	mpz_t(y); mpz_init(y);
+
 
 	printf("Type the number a: ");
-	scanf("%i", &a);
+	gmp_scanf("%Zd", a);
 	printf("Type the number b: ");
-	scanf("%i", &b);
+	gmp_scanf("%Zd", b);
 	printf("Type the number e: ");
-	scanf("%i", &e);
+	gmp_scanf("%Zd", e);
 	printf("Type the number n: ");
-	scanf("%i", &n);
+	gmp_scanf("%Zd", n);
 
-	r = a % b;
+	mpz_mod(r, a, b); //r = a % b;
 
 // Question 1
-	g = MDC(a,b);
+	
 
-	printf("The equation that we're looking for is: \n");
-	g = mdc_estendido(g, &x, &y, a, b);
-    printf("%i*%i + %i*%i = %i \n\n", a, x, b, y, g);
+	printf("\nQuestion 1: GCD of 'a' and 'b' for is: \n");
+	mdc_estendido(g, x, y, a, b);
+    gmp_printf("%Zd*%Zd + %Zd*%Zd = %Zd \t where the result is our GCD\n\n", a, x, b, y, g);
 
 //Question 2
-    printf("Modular Multiplicative Inverse exists: %i\n", inverso_modular(&r, a, n));
-    printf("r = %i\n\n", r);
+	if(inverso_modular(r, a, n) == 1){
+		printf("Question 2: \nModular Multiplicative Inverse DOES exists! :D\n");
+		gmp_printf("r = %Zd\n\n", r);
+	}
+	else{
+		printf("Question 2: \nModular Multiplicative Inverse DOES NOT exists :(\n\n");
+	}
 
 //Question 3
-    r = exp_binaria(r, b, e, n);
-    printf("The r number that we're looking that made b^e congruent to r (mod n): %i\n\n", r);
-   
+    exp_binaria(r, b, e, n);
+    gmp_printf("Question 3: \nThe r number that we're looking that made b^e congruent to r (mod n): %Zd\n\n", r);
+  
+    mpz_clear(a);
+	mpz_clear(b);
+	mpz_clear(e);
+	mpz_clear(g);
+	mpz_clear(n);
+	mpz_clear(r);
+	mpz_clear(x);
+	mpz_clear(y);
+
 	return 0;
 }
