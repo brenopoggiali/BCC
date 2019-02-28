c8a96bbc9e13d6cea78048a841a33a7cacc90643
brenopoggiali <brenopoggiali@gmail.com>
2019-02-27 23:22:10 -0300

    2019 first commit

---
+++ Programação Competitiva/Lista 1/1.c
@@ -0,0 +1,11 @@
+int main(int argc, char const *argv[])
+{
+  int a, b;
+
+  scanf("%i", &a);
+  scanf("%i", &b);
+  
+  printf("X = %i\n", a+b);
+
+  return 0;
+}
---
+++ Programação Competitiva/Lista 1/2.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool slump(char *string, int *aux){
+  if (string[*aux] != 'D' && string[*aux] != 'E'){
+    return false;
+  }
+  *aux += 1;
+  while (string[*aux] == 'F'){
+    *aux += 1;
+  } 
+  if(string[*aux] == 'G')
+      return true;
+  if (string[*aux] == 'D' || string[*aux] == 'E')
+    slump(string, aux);
+  else
+    return false;
+}
+
+bool slimp(char *string, int *aux){
+  if (string[*aux] != 'A')
+    return false;
+  *aux += 1;
+  if (string[*aux] == 'H')
+    return true;
+
+  while(string[*aux] == 'B' || string[*aux] == 'C' || string[*aux] == 'D' || string[*aux] == 'E'){
+    if (string[*aux] == 'C')
+      return true;
+    else if (string[*aux] == 'B'){
+      *aux += 1;
+      if(slimp(string, aux)){
+        *aux += 1;
+        if (string[*aux] == 'C')
+          return true;
+      }        
+    }
+    else if (string[*aux] == 'D' || string[*aux] == 'E'){
+      if(slump(string, aux)){
+        *aux += 1;
+        if (string[*aux] == 'C')
+          return true;
+      }
+    }
+    return false;
+  }
+  return false;
+}
+
+bool slurpy(char *string){
+  int aux = 0;
+  bool slimp_aux;
+  slimp_aux = slimp(string, &aux);
+  if(slimp_aux){
+    aux += 1;
+    if (slump(string, &aux)){
+      if (!string[aux+1])
+        return true;
+    }
+  }
+  return false;
+}
+
+
+int main(int argc, char const *argv[]){
+  int n;
+  scanf("%i", &n);
+  
+  char *line;
+  printf("SLURPYS OUTPUT\n");
+  for(int i=0; i<n; i++){
+    scanf("%s", line);
+    if (slurpy(line))
+      printf("YES\n");
+    else
+      printf("NO\n");
+  }
+  printf("END OF OUTPUT\n");
+
+  return 0;
+}
